60b19addae995bc78d5aac2e73ba10089ab392fa
Frank Charleston <colten_rocks@hotmail.com>
2025-02-22 07:50:30 -0600

    chore: fleshing out 1.0

--- content_scripts.js
+++ content_scripts.js
@@ -56,6 +56,36 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
+console.log("[DEBUG] Content script loaded");
+
+function scanAndDecrypt() {
+    console.log("[DEBUG] Scanning page for encrypted messages...");
+    document.querySelectorAll("*").forEach(element => {
+        const encryptedText = extractEncryptedText(element.innerText);
+        if (encryptedText) {
+            console.log("[DEBUG] Found encrypted text:", encryptedText);
+            let decryptedText = window.decryptText(encryptedText, prompt("Enter passphrase:", "mypassword"));
+            if (decryptedText) {
+                console.log("[DEBUG] Decrypted text:", decryptedText);
+                element.innerHTML = element.innerHTML.replace(
+                    `ENC[${encryptedText}]`,
+                    `<span class='decrypted-message' style='color: green;'>${decryptedText}</span>`
+                );
+            } else {
+                console.warn("[WARN] Decryption failed for:", encryptedText);
+            }
+        }
+    });
+}
+
+function extractEncryptedText(text) {
+    const match = text.match(/ENC\[(.*?)\]/);
+    return match ? match[1] : null;
+}
+
+document.addEventListener("DOMContentLoaded", scanAndDecrypt);
+
+
 // === 4) Dynamically load crypto-utils.js if needed ===
 async function loadCryptoUtils() {
   // If decryptText (or encryptText) is not defined, inject crypto-utils.js
